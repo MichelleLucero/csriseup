5447f403f3c8d5c6de501a11374e6a31a229f4a7
MichelleLucero <iammichellelucero@gmail.com>
2022-01-31 22:55:38 -0500

    token appears in request headers under authorization

--- src/app/token-interceptor.service.ts
+++ src/app/token-interceptor.service.ts
@@ -1,18 +1,29 @@
-// import { Injectable, Injector } from '@angular/core';
-// import { HttpInterceptor } from '@angular/common/http';
-// import { AuthService } from './auth/auth.service';
+import { Injectable, Injector } from '@angular/core';
+import {
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { AuthService } from './auth/auth.service';
+import { Observable } from 'rxjs';
 
-// @Injectable()
-// export class TokenInterceptorService implements HttpInterceptor {
-//   constructor(private injector: Injector) {}
+@Injectable({
+  providedIn: 'root',
+})
+export class TokenInterceptorService implements HttpInterceptor {
+  constructor(private inject: Injector) {}
 
-//   intercept(req: any, next: any) {
-//     let authService = this.injector.get(AuthService);
-//     let tokenizedReq = req.clone({
-//       setHeaders: {
-//         Authorization: `Bearer ${authService.getToken()}`,
-//       },
-//     });
-//     return next.handle(tokenizedReq);
-//   }
-// }
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    let authService = this.inject.get(AuthService);
+    let jwtToken = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${authService.getToken()}`,
+      },
+    });
+    return next.handle(jwtToken);
+  }
+}
