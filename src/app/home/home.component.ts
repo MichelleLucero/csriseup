34acd7bb7efd1271a68652c2bff175a4dbd0ebf3
MichelleLucero <iammichellelucero@gmail.com>
2022-02-01 13:31:39 -0500

    api request for user jobs works

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  jobs: any[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private userService: UserService) {
+    this.userService.getAllJobs().subscribe((job) => {
+      console.log(job);
+      this.jobs = job;
+      console.log(this.jobs);
+    });
   }
 
+  ngOnInit(): void {}
 }
