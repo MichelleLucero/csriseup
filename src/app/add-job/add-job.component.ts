2615ff9cebf248bd5cc5931e8203012a73689aba
MichelleLucero <iammichellelucero@gmail.com>
2022-02-02 21:28:41 -0500

    init form for add job

--- src/app/add-job/add-job.component.ts
+++ src/app/add-job/add-job.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-add-job',
@@ -6,7 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-job.component.css'],
 })
 export class AddJobComponent implements OnInit {
-  constructor() {}
+  myControl = new FormControl();
+  options: string[] = ['One', 'Two', 'Three'];
+  filteredOptions: any;
 
-  ngOnInit(): void {}
+  constructor(
+    @Inject(MAT_DIALOG_DATA) private data: any,
+    private matDialogRef: MatDialogRef<AddJobComponent>
+  ) {}
+
+  ngOnInit(): void {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map((value) => this._filter(value))
+    );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter((option) =>
+      option.toLowerCase().includes(filterValue)
+    );
+  }
+
+  onSubmit() {
+    this.matDialogRef.close();
+  }
 }
